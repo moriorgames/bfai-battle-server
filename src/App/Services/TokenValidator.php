a11cb9bf8114e7dc03efe4a8aee0cb0afd596737
Morior <moriorgames@gmail.com>
2018-07-03 06:35:33 +0200

    Add token validator service

--- src/App/Services/TokenValidator.php
+++ src/App/Services/TokenValidator.php
@@ -2,7 +2,12 @@
 
 namespace App\Services;
 
+use App\Definitions\Token;
+
 class TokenValidator
 {
-
+    public static function validate(string $token): bool
+    {
+        return Token::LENGTH === strlen($token);
+    }
 }
